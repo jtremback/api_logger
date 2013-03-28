a6688d196451db0b0ed396b91ed58ef1380f6ce5
Jehan Tremback <jehan.tremback@gmail.com>
2013-03-28 16:59:44 -0700

    logs depth successfully

--- server.js
+++ server.js
@@ -3,137 +3,88 @@
 //save to disk
 //Get api stored id onwards
 //repeat
+var balUtil = require('bal-util'),
+  http = require("http"),
+  fs = require('fs'),
+  spawn = require('child_process').spawn;
 
 
-var http = require("http");
-var fs = require('fs');
-
-
-// //Get mtgox api
-// //Get topmost id
-// //save to disk
-// //Get api stored id onwards
-// //repeat
-
-// var options = {
-//   host: 'data.mtgox.com',
-//   headers: {'user-agent': 'Mozilla/5.0'},
-//   path: '/api/1/BTCUSD/trades?raw'
-// };
-
-// http.get(options, function(res) {
-// 	res.setEncoding('utf8');
-// 	res.on('data', function(chunk){
-// 		chunkWrangler.add(chunk);
-// 	})
-// 	.on('end', function(){
-// 		chunkWrangler.end();
-// 	})
-// }).on('error', function(e) {
-//   console.log("Got error: " + e.message);
-// });
-
-// //make persistant container var
-// //if it is empty, set to chunk
-// //if it is fullsome, concatenate chunk on to it
-
-// var chunkWrangler = function() {
-// 	var wholethang = false;
-
-// 	return {
-// 		add: function(chunk) {
-// 			if (wholethang) {
-// 				wholethang += chunk;
-// 			} else {
-// 				wholethang = chunk;
-// 			}
-// 		},
-
-// 		end: function() {
-// 			var jason = JSON.parse(wholethang);
-// 			console.log(jason[1].tid);
-// 			wholethang = false;
-// 		}
-// 	}
-// }();
-
-
-//Get the transactions since the last id
+//Get the depth and log it
 //Trim and save them
-var logCycle = function(lasttid) {
+var logSnap = function(path) {
 
-	//In case its the first time round 
-	if (lasttid) { 
-		var path = '/api/1/BTCUSD/trades?since=' + lasttid
-	} else {
-		path = '/api/1/BTCUSD/trades?raw'
-	}
+  var options = {
+    host: 'data.mtgox.com',
+    headers: {'user-agent': 'Mozilla/5.0'},
+    path: path
+  };
 
-	var options = {
-	  host: 'data.mtgox.com',
-	  headers: {'user-agent': 'Mozilla/5.0'},
-	  path: path
-	};
-
-	http.get(options, function(res) {
-		res.setEncoding('utf8');
-		res.on('data', function(chunk){
-			chunkWrangler.add(chunk);
-		})
-		.on('end', function(){
-			chunkWrangler.end();
-		})
-	}).on('error', function(e) {
-	  console.log("Got error: " + e.message);
-	});
+  http.get(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      chunkWrangler.add(chunk);
+    })
+    .on('end', function() {
+      chunkWrangler.end();
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
 };
 
 //Assemble, process and append
 var chunkWrangler = function() {
-	var wholethang = false;
+  var wholethang = false;
 
-	return {
+  return {
 
-		//Assemble chunks into large string encoded object
-		add: function(chunk) {
-			if (wholethang) {
-				wholethang += chunk;
-			} else {
-				wholethang = chunk;
-			}
-		},
+    //Assemble chunks into large string encoded object
+    add: function(chunk) {
+      if (wholethang) {
+        wholethang += chunk;
+      } else {
+        wholethang = chunk;
+      }
+    },
 
-		//Turn into real object, process and append
-		end: function() {
-			var jason = JSON.parse(wholethang),
-				firstTid = jason[1].tid;
+    //Turn into real object
+    end: function() {
+      var json = JSON.parse(wholethang);
 
-			//Get only the properties needed
-			var summarizer = function(input) {
-				return {
-					tid: input.tid,
-					price: input.price,
-					amount: input.amount,
-					trade_type: input.trade_type
-				}
-			}
+      //Verify, summarize and append
+      var summarizer = function(depthArrayName, json) {
+        console.log(depthArrayName);
+        //Get only the properties needed
+        var summLogic = function(input) {
+          return {
+            stamp: input.stamp,
+            price: input.price,
+            amount: input.amount
+          }
+        };
+        var depthArray = json.return[depthArrayName];
+        return depthArray.map(summLogic);
+      }
 
-			//Turn back into string and append
-			var summarized = JSON.stringify(jason.map(summarizer)),
-				trimmed = summarized.slice(1, -1);
+      //Turn back into string and append
+      if (json.result === "success") {
+        var newObj = {}
+        newObj.asks = summarizer("asks", json);
+        newObj.bids = summarizer("bids", json);
 
-			
-			fs.appendFile('logg.txt', trimmed, function (err) {
-				if (err) throw err;
-				console.log('The "data to append" was appended to file!');
-				setTimeout(logCycle(firstTid), 1000);
-			});
-			
-			console.log(firstTid);
+        var summarized = JSON.stringify(newObj),
+          newlined = summarized.replace(/,/g, ',\n');
 
-		}
-	}
+        fs.appendFile('log.txt', newlined, function (err) {
+          if (err) throw err;
+          console.log('The "data to append" was appended to file!');
 
+        });
+      } else {
+        console.log("something wrong with api");
+      }
+    }
+  }
 }();
 
-logCycle(0);
+logSnap("/api/1/BTCUSD/depth/fetch");
